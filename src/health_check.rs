a7ac84efc47df545ad6fcf8683235bfac53825c6
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2025-07-13 16:39:56 -0700

    Remove random logic

--- src/health_check.rs
+++ src/health_check.rs
@@ -1,15 +1,8 @@
 use actix_web::{get, Responder, HttpResponse};
-use rand::Rng;
+
 
 #[get("/admin/health")]
 pub async fn admin_health_check() -> impl Responder {
-    let mut rng = rand::rng();
-    let bool_rand = rng.random_bool(0.5);
 
-    if bool_rand {
-        HttpResponse::Ok().body("Ok")
-    } else {
-        HttpResponse::InternalServerError().finish()
-    }
-
+    HttpResponse::InternalServerError().finish()
 }
