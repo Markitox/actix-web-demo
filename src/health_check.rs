79d198d7bc5b59b9cefe5dfa77321de708c77420
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2025-07-13 16:17:07 -0700

    Return random errors in health check endpoint

--- src/health_check.rs
+++ src/health_check.rs
@@ -1,6 +1,15 @@
 use actix_web::{get, Responder, HttpResponse};
+use rand::Rng;
 
 #[get("/admin/health")]
 pub async fn admin_health_check() -> impl Responder {
-    HttpResponse::Ok().body("Ok")
+    let mut rng = rand::rng();
+    let bool_rand = rng.random_bool(0.5);
+
+    if bool_rand {
+        HttpResponse::Ok().body("Ok")
+    } else {
+        HttpResponse::InternalServerError().finish()
+    }
+
 }
