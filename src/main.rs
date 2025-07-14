d72c772abddff51ddf5fa95dc41e1c77300a5d2d
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2025-07-13 18:58:13 -0700

    Add route to show environment variables

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{ web, App, HttpServer };
 
 use crate::hello_world_controller::{hello, echo, hello_world, manual_hello};
 use crate::computer_controller::{index, save};
-use crate::health_check::admin_health_check;
+use crate::health_check::{admin_health_check, admin_env_route};
 
 
 mod hello_world_controller;
@@ -26,6 +26,7 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .service(hello_world)
             .service(admin_health_check)
+            .service(admin_env_route)
             .route("/hey", web::get().to(manual_hello))
         })
     .bind("0.0.0.0:8080").unwrap()
